fd4a29f896f4d651f7c99ed0395937e69ecd3f77
Linux-XP <145774158+Linux-XP@users.noreply.github.com>
2023-10-21 15:43:40 -0300

    Update converter-v2.sh

--- converter-v2.sh
+++ converter-v2.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Re-encode videos to a target size in MB. Supports individual files or batch processing of a directory.
@@ -39,7 +38,7 @@ if [ ! -e "$INPUT_PATH" ]; then
     exit 1
 fi
 
-if ! [[ "$T_SIZE" =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+if ! [[ "$T_SIZE" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
     echo "Error: Invalid target size! Please provide a valid number."
     exit 1
 fi
@@ -47,7 +46,7 @@ fi
 # Function to process a single video file
 process_video() {
     local INPUT_FILE="$1"
-    T_FILE="${INPUT_FILE%.*}-$T_SIZE""MB.mp4"
+    T_FILE="${INPUT_FILE%.*}-${T_SIZE}MB.mp4"
 
     echo "Processing: $INPUT_FILE"
 
@@ -64,33 +63,30 @@ process_video() {
     T_MINSIZE=$(awk -v arate="$O_ARATE" -v duration="$O_DUR" 'BEGIN { printf "%.2f", ( (arate * duration) / 8192 ) }')
 
     # Check if the target size is acceptable
-    IS_MINSIZE=$(awk -v size="$T_SIZE" -v minsize="$T_MINSIZE" 'BEGIN { print (minsize < size) }')
-
-    # Error message if size is too small
-    if [[ $IS_MINSIZE -eq 0 ]]; then
+    if (( $(awk 'BEGIN {print ('"$T_MINSIZE"' < '"$T_SIZE"')}') )); then
         echo "Error: Target size ${T_SIZE}MB is too small for file $INPUT_FILE!"
         echo "Try values larger than ${T_MINSIZE}MB"
-        return
+        exit 1  # Termina o script com um cdigo de erro
     fi
 
     # Set target audio bitrate
     T_ARATE=$O_ARATE
 
     # Calculate target video rate
-    T_VRATE=$(awk -v size="$T_SIZE" -v duration="$O_DUR" -v audio_rate="$O_ARATE" 'BEGIN { print  ( ( size * 8192.0 ) / ( 1.048576 * duration ) - audio_rate) }')
+    T_VRATE=$(awk -v size="$T_SIZE" -v duration="$O_DUR" -v audio_rate="$O_ARATE" 'BEGIN { printf "%.0f", ( ( size * 8192.0 ) / ( 1.048576 * duration ) - audio_rate) }')
 
     # Construct ffmpeg command
-    CMD="ffmpeg -y -i "$INPUT_FILE" -c:v $VIDEO_CODEC -b:v "$T_VRATE"k"
+    CMD="ffmpeg -y -i \"$INPUT_FILE\" -c:v $VIDEO_CODEC -b:v ${T_VRATE}k"
 
     # Add optional framerate and resolution parameters
     [ ! -z "$FRAMERATE" ] && CMD="$CMD -r $FRAMERATE"
     [ ! -z "$RESOLUTION" ] && CMD="$CMD -s $RESOLUTION"
 
     if [[ $PASSES -eq 1 ]]; then
-        CMD="$CMD -c:a $AUDIO_CODEC -b:a "$T_ARATE"k "$T_FILE""
+        CMD="$CMD -c:a $AUDIO_CODEC -b:a ${T_ARATE}k \"$T_FILE\""
         eval "$CMD"
     else
-        CMD="$CMD -pass 1 -an -f mp4 /dev/null && ffmpeg -i "$INPUT_FILE" -c:v $VIDEO_CODEC -b:v "$T_VRATE"k -pass 2 -c:a $AUDIO_CODEC -b:a "$T_ARATE"k "$T_FILE""
+        CMD="$CMD -pass 1 -an -f mp4 /dev/null && ffmpeg -i \"$INPUT_FILE\" -c:v $VIDEO_CODEC -b:v ${T_VRATE}k -pass 2 -c:a $AUDIO_CODEC -b:a ${T_ARATE}k \"$T_FILE\""
         eval "$CMD"
     fi
 
